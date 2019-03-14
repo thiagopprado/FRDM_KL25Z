9ad33816de33146638cb3b61190abaf7d18f60dd
Thiago Pereira do Prado <thiagopereiraprado@gmail.com>
2019-03-14 18:03:56 -0300

    Add files via upload

---
+++ Embarcados/SistemaDinamico/Debug/lib/adc.d
@@ -0,0 +1,25 @@
+lib/adc.o lib/adc.d: \
+ C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/adc.c \
+ C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/adc.h \
+ ../CMSIS/fsl_device_registers.h ../CMSIS/MKL25Z4.h \
+ ../CMSIS/core_cm0plus.h ../CMSIS/core_cmInstr.h ../CMSIS/cmsis_gcc.h \
+ ../CMSIS/core_cmFunc.h ../CMSIS/system_MKL25Z4.h \
+ ../CMSIS/MKL25Z4_features.h
+
+C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/adc.h:
+
+../CMSIS/fsl_device_registers.h:
+
+../CMSIS/MKL25Z4.h:
+
+../CMSIS/core_cm0plus.h:
+
+../CMSIS/core_cmInstr.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/core_cmFunc.h:
+
+../CMSIS/system_MKL25Z4.h:
+
+../CMSIS/MKL25Z4_features.h:
---
+++ Embarcados/SistemaDinamico/Debug/lib/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../lib/Sistema.c \
+../lib/adc.c \
+../lib/dac.c \
+../lib/gpio_x.c 
+
+OBJS += \
+./lib/Sistema.o \
+./lib/adc.o \
+./lib/dac.o \
+./lib/gpio_x.o 
+
+C_DEPS += \
+./lib/Sistema.d \
+./lib/adc.d \
+./lib/dac.d \
+./lib/gpio_x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+lib/Sistema.o: C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/Sistema.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DFSL_RTOS_FREE_RTOS -DSDK_OS_FREE_RTOS -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../lib -I../source -I../ -I../freertos -I../drivers -I../CMSIS -I../utilities -I../startup -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib/adc.o: C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/adc.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DFSL_RTOS_FREE_RTOS -DSDK_OS_FREE_RTOS -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../lib -I../source -I../ -I../freertos -I../drivers -I../CMSIS -I../utilities -I../startup -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib/dac.o: C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/dac.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DFSL_RTOS_FREE_RTOS -DSDK_OS_FREE_RTOS -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../lib -I../source -I../ -I../freertos -I../drivers -I../CMSIS -I../utilities -I../startup -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lib/gpio_x.o: C:/Users/thiag/Google\ Drive/UNIFEI/Atividades\ Complementares/Artigos/Eq\ Diff/SistemaDinamico/lib/gpio_x.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DSDK_OS_BAREMETAL -DFSL_RTOS_BM -DSDK_DEBUGCONSOLE=0 -DCR_INTEGER_PRINTF -DPRINTF_FLOAT_ENABLE=0 -DFSL_RTOS_FREE_RTOS -DSDK_OS_FREE_RTOS -DCPU_MKL25Z128VLK4 -DCPU_MKL25Z128VLK4_cm0plus -D__MCUXPRESSO -D__USE_CMSIS -DDEBUG -I../board -I../lib -I../source -I../ -I../freertos -I../drivers -I../CMSIS -I../utilities -I../startup -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
