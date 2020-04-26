b8af1f39e96ebc88bcd74ad5ef7326878971124d
Thiago Pereira do Prado <thiagopereiraprado@gmail.com>
2020-04-25 21:28:33 -0300

    [LCD_I2C] Added functions prototipes and updated I2C structure fields

--- drivers/lcd_i2c/src/lcd_i2c.c
+++ drivers/lcd_i2c/src/lcd_i2c.c
@@ -7,52 +7,9 @@ uint8_t msg = 0;
 i2c_msg_t lcd_i2c_msg = { 0 };
 I2C_p i2c_ptr = NULL;
 
-static inline void delay_10us(void) {
-    uint16_t i = 0;
-    for (i = 0; i < 40; i++);
-}
-
-static inline void delay_5ms(void) {
-    uint16_t i = 0;
-    for (i = 0; i < 500; i++) {
-        delay_10us();
-    }
-}
-
-static inline lcd_i2c_err_t lcd_i2c_pulse(uint8_t data) {
-    i2c_err_t i2c_err = I2C_ERR_NO_ERROR;
-    uint8_t i = 0;
-
-    // Write Data + LED ON
-    msg = data | LCD_I2C_L;
-    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
-    if (i2c_err != I2C_ERR_NO_ERROR) {
-        return LCD_I2C_ERR_TRX_FAIL;
-    }
-    delay_10us();
-
-    msg |= LCD_I2C_E; // Enable Pulse
-    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
-    if (i2c_err != I2C_ERR_NO_ERROR) {
-        return LCD_I2C_ERR_TRX_FAIL;
-    }
-
-    delay_10us();
-
-    // Write Data + LED ON
-    msg = data | LCD_I2C_L;
-    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
-    if (i2c_err != I2C_ERR_NO_ERROR) {
-        return LCD_I2C_ERR_TRX_FAIL;
-    }
-
-    // Delay 50us
-    for (i = 0; i < 5; i++) {
-        delay_10us();
-    }
-
-    return LCD_I2C_ERR_NO_ERROR;
-}
+static inline void delay_5ms(void);
+static inline void delay_10us(void);
+static inline lcd_i2c_err_t lcd_i2c_pulse(uint8_t data);
 
 lcd_i2c_err_t lcd_i2c_init (uint8_t i2c_addr, I2C_p i2c_p) {
     i2c_err_t i2c_err = I2C_ERR_NO_ERROR;
@@ -65,8 +22,9 @@ lcd_i2c_err_t lcd_i2c_init (uint8_t i2c_addr, I2C_p i2c_p) {
 
     i2c_ptr = i2c_p;
     lcd_i2c_msg.i2c_addr = i2c_addr;
-    lcd_i2c_msg.message = &msg;
-    lcd_i2c_msg.size = LCD_I2C_TRX_SIZE;
+    lcd_i2c_msg.tx_data = &msg;
+    lcd_i2c_msg.tx_size = LCD_I2C_TRX_SIZE;
+    lcd_i2c_msg.read = false;
 
     // Wait 50ms for LCD to start
     for (i = 0; i < 10; i++) {
@@ -162,4 +120,50 @@ lcd_i2c_err_t lcd_i2c_data (uint8_t data) {
 	lcd_i2c_err = lcd_i2c_pulse((data << 4) | LCD_I2C_RS);
 
 	return lcd_i2c_err;
+}
+
+static inline lcd_i2c_err_t lcd_i2c_pulse(uint8_t data) {
+    i2c_err_t i2c_err = I2C_ERR_NO_ERROR;
+    uint8_t i = 0;
+
+    // Write Data + LED ON
+    msg = data | LCD_I2C_L;
+    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
+    if (i2c_err != I2C_ERR_NO_ERROR) {
+        return LCD_I2C_ERR_TRX_FAIL;
+    }
+
+    msg |= LCD_I2C_E; // Enable Pulse
+    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
+    if (i2c_err != I2C_ERR_NO_ERROR) {
+        return LCD_I2C_ERR_TRX_FAIL;
+    }
+
+    delay_10us();
+
+    // Write Data + LED ON
+    msg = data | LCD_I2C_L;
+    i2c_err = i2c_write(i2c_ptr, &lcd_i2c_msg);
+    if (i2c_err != I2C_ERR_NO_ERROR) {
+        return LCD_I2C_ERR_TRX_FAIL;
+    }
+
+    // Delay 50us
+    for (i = 0; i < 5; i++) {
+        delay_10us();
+    }
+
+    return LCD_I2C_ERR_NO_ERROR;
+}
+
+static inline void delay_5ms(void) {
+    uint16_t i = 0;
+    for (i = 0; i < 500; i++) {
+        delay_10us();
+    }
+}
+
+static inline void delay_10us(void) {
+    uint16_t i = 0;
+    for (i = 0; i < 40; i++);
 }
